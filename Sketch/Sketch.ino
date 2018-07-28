4b5940b18bb4c40b4c8ebe92e372896bf03d30e8
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-07-28 17:07:23 +0200

    Nothing is working.

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -3,17 +3,17 @@
 DigiMIDIDevice midi;
 
 void setup() {
- pinMode(1,OUTPUT); //Pitch is output
- pinMode(5, OUTPUT); //Gate is output
- 
- // Setup Timer1 to do PWM DAC for Pitch.
- TCCR1 = _BV(PWM1A) | _BV(COM1A1) | _BV(CS10);
- GTCCR = 0;
- OCR1C = 239;
- OCR1A = 0; // Set initial Pitch to C2.
- digitalWrite(5,LOW); // Set initial Gate to LOW.
+  pinMode(9, OUTPUT); // Pitch is output.
+  pinMode(3, OUTPUT); // Gate is output.
+
+  // Setup Timer1 to do PWM DAC for Pitch.
+  TCCR1A = _BV(COM1A1) | _BV(CS10);
+  GTCCR = 0;
+  OCR1B = 239;
+  OCR1A = 0; // Set initial Pitch to C2.
+  digitalWrite(3, LOW); // Set initial Gate to LOW.
 }
 
 void loop() {
- midi.update(); // Check if any MIDI data is received.
+  midi.update(); // Check if any MIDI data is received.
 }
