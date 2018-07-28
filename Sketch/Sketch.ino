40401d05509c8621b8f96ed7e29a68587a8e92d8
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-07-28 16:04:02 +0200

    Written sketch.

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -1,17 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DigiMIDI.h>
 
-pinMode(1,OUTPUT); //Pitch is output
-pinMode(5, OUTPUT); //Gate is output
-//Setup Timer1 to do PWM DAC for Pitch
-TCCR1 = _BV(PWM1A) | _BV(COM1A1) | _BV(CS10);
-GTCCR = 0;
-OCR1C = 239;
-OCR1A = 0; //Set initial Pitch to C2
-digitalWrite(5,LOW); //Set initial Gate to LOW;
+DigiMIDIDevice midi;
+
+void setup() {
+ pinMode(1,OUTPUT); //Pitch is output
+ pinMode(5, OUTPUT); //Gate is output
+ 
+ // Setup Timer1 to do PWM DAC for Pitch.
+ TCCR1 = _BV(PWM1A) | _BV(COM1A1) | _BV(CS10);
+ GTCCR = 0;
+ OCR1C = 239;
+ OCR1A = 0; // Set initial Pitch to C2.
+ digitalWrite(5,LOW); // Set initial Gate to LOW.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ midi.update(); // Check if any MIDI data is received.
 }
